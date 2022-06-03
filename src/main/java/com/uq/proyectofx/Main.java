8fd402e5285fb1af5d190bec48985200c50df01d
Jhonatan Gomez <jhonatanva.2003.jsgv@gmail.com>
2022-06-02 22:34:48 -0500

    Project

--- src/main/java/com/uq/proyectofx/Main.java
+++ src/main/java/com/uq/proyectofx/Main.java
@@ -14,6 +14,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
+
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
